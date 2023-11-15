46a1ca2404d5373c1f06c0484f8c87c694f875d6
KatyN <e.nikonova.0407@gmail.com>
2023-11-15 12:45:02 +0300

    Update after review

--- homework_02/conftest.py
+++ homework_02/conftest.py
@@ -4,7 +4,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_configs():
-    browser.config.window_width = 1200
+    browser.config.window_width = 1280
     browser.config.window_height = 720
     browser.config.browser_name = 'chrome'
     yield
